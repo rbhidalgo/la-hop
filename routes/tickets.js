2311b3fec21d80f02b70bccbad3d5a3dfacb26b2
chaselancaster <chaseblancaster@gmail.com>
2019-06-11 15:44:20 -0700

    create post route in tickets.js to create a ticket

--- routes/tickets.js
+++ routes/tickets.js
@@ -2,3 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const Trade = require("../models/Ticket");
+
+// Creating a ticket request
+router.post("/create", async (req, res) => {
+  console.log("create post route hit");
+  try {
+    const createdTicket = await Ticket.create(req.body);
+    if (createdTicket) {
+      res.json({
+        ticket: createdTicket,
+        success: true,
+        message: "Ticket successfully submitted."
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
