fa0263e0796c16868bb43996069c96d1996709fe
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-06-11 15:21:05 -0700

    map container added

---
+++ src/components/MapContainer/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MapContainer extends Component {
+
+    render() {
+        return(
+            <>
+            <h1>Map Container</h1>
+            </>
+        )
+    }
+
+}
+
+export default MapContainer;
