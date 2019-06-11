6f3626b32d52eb878e6b7c8985a1b742452aa5e6
chaselancaster <chaseblancaster@gmail.com>
2019-06-11 15:34:08 -0700

    create TicketSchema in Ticket.js

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,1 +1,18 @@
 const mongoose = require("mongoose");
+
+const TicketSchema = new mongoose.Schema({
+  address: { type: String, required: true },
+  local: { type: String },
+  date: { type: Date, required: true },
+  peopleCount: { type: Number, required: true },
+  peopleNames: { type: String },
+  phyiscalDescript: { type: String, required: true },
+  needsDescript: { type: String },
+  tags: [{ type: String }],
+  name: { type: String },
+  org: { type: String },
+  selfDescript: { type: String },
+  email: { type: String },
+  phone: { type: String },
+  agreement: { type: Boolean }
+});
