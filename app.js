f3d684ef97dfcaf0baaf6c4175be9bbaff17678b
chaselancaster <chaseblancaster@gmail.com>
2019-06-11 14:18:16 -0700

    remove requirement for cors

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT;
 
