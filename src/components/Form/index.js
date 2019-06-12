62d61882b810feadbb5dad09a0e39b2363b6baba
Levi Winans <levieiko@gmail.com>
2019-06-11 21:44:10 -0700

    color match, headher height, nav height set

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,25 +4,8 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
 // // import './Register.css'
-const Input = ({onSubmit}) => 
-    <section  className="enter">
-        <form onSubmit={e => onSubmit(e)}>
-            <textarea type="text" placeholder="location" name="location"></textarea>
-            <input type="date" placeholder="date" name="date"></input>
-            <input type="text" placeholder="peopleCount" name="peopleCount"></input>
-            <input type="text" placeholder="peopleNames" name="peopleNames"></input>
-            <textarea type="text" placeholder="pyshicalDescript" name="pyshicalDescript"></textarea>
-            <textarea type="text" placeholder="needsDescript" name="needsDescript"></textarea>
-            <input type="text" placeholder="tags" name="tags"></input>
-            <input type="text" placeholder="name" name="name"></input>
-            <input type="text" placeholder="org" name="org"></input>
-            <textarea type="text" placeholder="selfDescript" name="selfDescript"></textarea>
-            <input type="text" placeholder="email" name="email"></input>
-            <input type="text" placeholder="phone" name="phone"></input>
-            <input type="checkbox" name="agreement"></input>
-            <button type="submit">Submit</button>
-        </form>
-    </section>
+
+
 
 class Form extends Component {
 
@@ -42,11 +25,32 @@ class Form extends Component {
         // })
     }
 
+
+    // var curr = new Date();
+    // curr.setDate(curr.getDate() + 3);
+    // var date = curr.toISOString().substr(0,10);
+    // <input id="dateRequired" type="date" name="dateRequired" defaultValue={date} /> 
+
     render(){
         const { message } = this.state
         return(
             <>
-                <Input />
+                <form onSubmit={e => this.onSubmit(e)}>
+                    <textarea type="text" placeholder="location" name="location"></textarea>
+                    <input type="date" placeholder="date" name="date"></input>
+                    <input type="text" placeholder="peopleCount" name="peopleCount"></input>
+                    <input type="text" placeholder="peopleNames" name="peopleNames"></input>
+                    <textarea type="text" placeholder="pyshicalDescript" name="pyshicalDescript"></textarea>
+                    <textarea type="text" placeholder="needsDescript" name="needsDescript"></textarea>
+                    <input type="text" placeholder="tags" name="tags"></input>
+                    <input type="text" placeholder="name" name="name"></input>
+                    <input type="text" placeholder="org" name="org"></input>
+                    <textarea type="text" placeholder="selfDescript" name="selfDescript"></textarea>
+                    <input type="text" placeholder="email" name="email"></input>
+                    <input type="text" placeholder="phone" name="phone"></input>
+                    <input type="checkbox" name="agreement"></input>
+                    <button type="submit">Submit</button>
+                </form>
             </>
         )
     }
