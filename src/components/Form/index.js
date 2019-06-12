13794e0839cda2ac91b5a28c5fce8e40a56466f9
chaselancaster <chaseblancaster@gmail.com>
2019-06-11 17:55:04 -0700

    Create onSubmit function that creates a ticket

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -11,21 +11,30 @@ class Form extends Component {
 
   onSubmit = async e => {
     e.preventDefault();
-    // const registerResponse = await fetch(#########################, {
-    //     method: 'POST',
-    //     credentials: 'include',
-    //     body: JSON.stringify(this.state),
-    //     headers: {
-    //         'Content-type' : 'application/json'
-    //     }
-    // })
+    const createTicket = await fetch("http://localhost:3001/ticket/create", {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify(this.state),
+      headers: {
+        "Content-type": "application/json"
+      }
+    });
+    const parsedResponse = await createTicket.json();
+    if (parsedResponse.success) {
+      // setting localStorage on front end so user remains logged in
+      // logout = localStorage.clear()
+      // and clear sessions in back
+      this.setState({
+        message: parsedResponse.message
+      });
+    } 
   };
 
   render() {
     const { message } = this.state;
     return (
     <> 
-        <form>
+        <form onSubmit={this.onSubmit}>
             <textarea type="text" placeholder="location" name="location" />
             <input type="date" placeholder="date" name="date" />
             <input type="text" placeholder="peopleCount" name="peopleCount" />
