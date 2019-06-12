0c2c53d62de379f72e914c6926e5c79688e0be41
chaselancaster <chaseblancaster@gmail.com>
2019-06-11 21:40:28 -0700

    clear form when it is submitted

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -45,7 +45,20 @@ class Form extends Component {
             // logout = localStorage.clear()
             // and clear sessions in back
             this.setState({
-              message: parsedResponse.message
+              message: parsedResponse.message,
+              location: '',
+              date: '',
+              peopleCount: 0,
+              peopleNames: '',
+              physicalDescript: '',
+              needsDescript: '',
+              tags: [],
+              name: '',
+              org: '',
+              selfDescript: '',
+              email: '',
+              phone: '',
+              agreement: false
             });
           } 
     } catch(err) {
@@ -55,28 +68,29 @@ class Form extends Component {
   };
 
   render() {
-    const { message } = this.state;
+    const { message, location, date, peopleCount, peopleNames, physicalDescript, needsDescript, tags, name, org, selfDescript, email, phone, agreement } = this.state;
     return (
     <> 
-        {/* <h1>{message}</h1> */}
+        <h1>{message}</h1>
         <form onSubmit={this.onSubmit}>
-            <textarea type="text" placeholder="location" name="location" onChange={this.changeHandler}/>
-            <input type="date" placeholder="date" name="date" onChange={this.changeHandler}/>
-            <input type="text" placeholder="peopleCount" name="peopleCount" onChange={this.changeHandler}/>
-            <input type="text" placeholder="peopleNames" name="peopleNames" onChange={this.changeHandler}/>
+            <textarea type="text" placeholder="location" name="location" onChange={this.changeHandler} value={location}/>
+            <input type="date" placeholder="date" name="date" onChange={this.changeHandler} value={date}/>
+            <input type="text" placeholder="peopleCount" name="peopleCount" onChange={this.changeHandler} value={peopleCount}/>
+            <input type="text" placeholder="peopleNames" name="peopleNames" onChange={this.changeHandler} value={peopleNames}/>
             <textarea
                 type="text"
                 placeholder="pyshicalDescript"
                 name="physicalDescript"
                 onChange={this.changeHandler}
+                value={physicalDescript}
             />
-            <textarea type="text" placeholder="needsDescript" name="needsDescript" onChange={this.changeHandler}/>
-            <input type="text" placeholder="tags" name="tags" />
-            <input type="text" placeholder="name" name="name" onChange={this.changeHandler}/>
-            <input type="text" placeholder="org" name="org" onChange={this.changeHandler}/>
-            <textarea type="text" placeholder="selfDescript" name="selfDescript" onChange={this.changeHandler}/>
-            <input type="text" placeholder="email" name="email" onChange={this.changeHandler}/>
-            <input type="text" placeholder="phone" name="phone" onChange={this.changeHandler}/>
+            <textarea type="text" placeholder="needsDescript" name="needsDescript" onChange={this.changeHandler} value={needsDescript}/>
+            <input type="text" placeholder="tags" name="tags"/>
+            <input type="text" placeholder="name" name="name" onChange={this.changeHandler} value={name}/>
+            <input type="text" placeholder="org" name="org" onChange={this.changeHandler} value={org}/>
+            <textarea type="text" placeholder="selfDescript" name="selfDescript" onChange={this.changeHandler} value={selfDescript}/>
+            <input type="text" placeholder="email" name="email" onChange={this.changeHandler} value={email}/>
+            <input type="text" placeholder="phone" name="phone" onChange={this.changeHandler} value={phone}/>
             <input type="checkbox" name="agreement" />
             <button type="submit">Submit</button>
         </form>
