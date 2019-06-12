133fe295c6ce9c331ace5a90e04538674f1c013c
chaselancaster <chaseblancaster@gmail.com>
2019-06-12 16:42:25 -0700

    Change color of progress bar in ProgressBar.css

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -2,13 +2,25 @@ import React, { Component } from "react";
 import "react-step-progress-bar/styles.css";
 import { ProgressBar, Step } from "react-step-progress-bar";
 
+import './ProgressBar.css'
+
 class StepProgressBar extends Component {
     state = {
         percent: 0
     }
+
+    changeProgress = () => {
+        console.log("changeProgress function hit")
+        let currentNum = this.state.percent
+        this.setState({
+            percent: currentNum + 50
+        });
+    }
+
   render() {
      const { percent } = this.state
     return (
+        <div>
         <ProgressBar percent={percent}>
         <Step>
           {({ accomplished, index }) => (
@@ -38,6 +50,8 @@ class StepProgressBar extends Component {
           )}
         </Step>
       </ProgressBar>
+      <button onClick={this.changeProgress}>Next Step</button>
+      </div>
     );
   }
 }
