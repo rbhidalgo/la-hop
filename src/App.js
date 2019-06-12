d3cb0ab572baf8822f834f9db4ab740db8f22e62
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-06-12 10:00:03 -0700

    search bar in progress

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
             <LearnMore />
           }/>
           <Route exact path={routes.REQUEST} render={() => <>
-            <MapContainer />
+            <MapContainer/>
               <p>A nice warm laptop for me to sit on get my claw stuck in the dog's ear skid on floor, crash into wall and stares at human while pushing stuff off a table jump off balcony, onto stranger's head or meow all night, for claw drapes. Hack up furballs spill litter box, scratch at owner, destroy all furniture, especially couch, yet please stop looking at your phone and pet me kitty scratches couch bad kitty claw at curtains stretch and yawn nibble on tuna ignore human bite human hand. Jump off balcony, onto stranger's head sleep everywhere, but not in my bed cat sit like bread yet my left donut is missing, as is my right. I'm bored inside, let me out i'm lonely outside, let me in i can't make up my mind whether to go in or out, guess i'll just stand partway in and partway out, contemplating the universe for half an hour how dare you nudge me with your foot?!?! leap into the air in greatest offense!. Go into a room to decide you didn't want to be in there anyway i bet my nine lives on you-oooo-ooo-hooo and leave fur on owners clothes, and terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not sorry but purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table kick up litter or ignore the human until she needs to get up, then climb on her lap and sprawl. 
               </p>
             </> }/>
--- src/components/MapContainer/index.js
+++ src/components/MapContainer/index.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import {Map, Marker, GoogleApiWrapper, InfoWindow} from 'google-maps-react';
+import {Map, Marker, GoogleApiWrapper, GroundOverlay} from 'google-maps-react';
 
 export class MapContainer extends Component {
  render() {
    return (
-     <Map google={this.props.google} zoom={14} defaultCenter={{ lat: 34.0522, lng: 118.2437 }}>
+     
+     <Map google={this.props.google} zoom={10} initialCenter={{ lat: 34.0522, lng: -118.2437  }} style={{ height: '40%', width: '60%'}}>
 
     <Marker onClick={this.onMarkerClick}
                name={'Current location'} />
 
-       <InfoWindow onClose={this.onInfoWindowClose}>
-           <div>
-             {/* <h1>{this.state.selectedPlace.name}</h1> */}
-           </div>
-       </InfoWindow>
+
      </Map>
    );
  }
 }
 
 export default GoogleApiWrapper({
- apiKey: ('AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg')
-})(MapContainer)
+ apiKey: ('AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg')})(MapContainer)
