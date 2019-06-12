97feff00a6d52edbc358de46c605ad8d6f2a2e10
chaselancaster <chaseblancaster@gmail.com>
2019-06-11 17:35:04 -0700

    delete functon holding the form. Move the form to return

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,30 +2,7 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 
 // // import './Register.css'
-const Input = ({ onSubmit }) => (
-  <section className="enter">
-    <form onSubmit={e => onSubmit(e)}>
-      <textarea type="text" placeholder="location" name="location" />
-      <input type="date" placeholder="date" name="date" />
-      <input type="text" placeholder="peopleCount" name="peopleCount" />
-      <input type="text" placeholder="peopleNames" name="peopleNames" />
-      <textarea
-        type="text"
-        placeholder="pyshicalDescript"
-        name="pyshicalDescript"
-      />
-      <textarea type="text" placeholder="needsDescript" name="needsDescript" />
-      <input type="text" placeholder="tags" name="tags" />
-      <input type="text" placeholder="name" name="name" />
-      <input type="text" placeholder="org" name="org" />
-      <textarea type="text" placeholder="selfDescript" name="selfDescript" />
-      <input type="text" placeholder="email" name="email" />
-      <input type="text" placeholder="phone" name="phone" />
-      <input type="checkbox" name="agreement" />
-      <button type="submit">Submit</button>
-    </form>
-  </section>
-);
+
 
 class Form extends Component {
   state = {
@@ -47,9 +24,29 @@ class Form extends Component {
   render() {
     const { message } = this.state;
     return (
-      <>
-        <Input />
-      </>
+    <> 
+        <form>
+            <textarea type="text" placeholder="location" name="location" />
+            <input type="date" placeholder="date" name="date" />
+            <input type="text" placeholder="peopleCount" name="peopleCount" />
+            <input type="text" placeholder="peopleNames" name="peopleNames" />
+            <textarea
+                type="text"
+                placeholder="pyshicalDescript"
+                name="pyshicalDescript"
+            />
+            <textarea type="text" placeholder="needsDescript" name="needsDescript" />
+            <input type="text" placeholder="tags" name="tags" />
+            <input type="text" placeholder="name" name="name" />
+            <input type="text" placeholder="org" name="org" />
+            <textarea type="text" placeholder="selfDescript" name="selfDescript" />
+            <input type="text" placeholder="email" name="email" />
+            <input type="text" placeholder="phone" name="phone" />
+            <input type="checkbox" name="agreement" />
+            <button type="submit">Submit</button>
+        </form>
+  
+    </>
     );
   }
 }
