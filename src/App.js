3bed88c7a8e10beac625d3cea2e902a24a5a92a7
Levi Winans <levieiko@gmail.com>
2019-06-13 15:19:48 -0700

    fixed locator

--- src/App.js
+++ src/App.js
@@ -147,10 +147,9 @@ class App extends Component {
             <div className="barContainer">
             <StepProgressBar percent={this.state.percent}/>
             <br />
-
+            <br />
             </div>
-            <hr />
-            <hr />
+
             
             <StepOne changeHandler={this.changeHandler} location={this.state.location} changeProgress={this.changeProgress}/>
 
