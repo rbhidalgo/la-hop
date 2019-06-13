40ef286ad4a4e1a91f4f0fa9f0f11dc55ff42ad1
chaselancaster <chaseblancaster@gmail.com>
2019-06-12 21:56:30 -0700

    import TagButtons to StepTwo

--- src/components/StepTwo/index.js
+++ src/components/StepTwo/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import TagButtons from 'react-tag-buttons';
 
 const tagsList = [
     {id:1, text:"Clothing"},
@@ -14,8 +15,8 @@ class StepTwo extends Component {
             <div>
                 <h1>This is the StepTwo Component</h1>
                 <form>
-                    <input type="text" placeholder="peopleCount" name="peopleCount" onChange={this.props.changeHandler} value={peopleCount}/>
-                    <input type="text" placeholder="peopleNames" name="peopleNames" onChange={this.props.changeHandler} value={peopleNames}/>
+                    <input type="text" placeholder="peopleCount" name="peopleCount" onChange={this.props.changeHandler} value={this.props.peopleCount}/>
+                    <input type="text" placeholder="peopleNames" name="peopleNames" onChange={this.props.changeHandler} value={this.props.peopleNames}/>
                     <textarea type="text" placeholder="physicalDescript" name="physicalDescript" onChange={this.props.changeHandler} value={physicalDescript}/>
                     <TagButtons
                     dataSource={tagsList}
