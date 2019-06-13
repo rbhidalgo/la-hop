afc5ac964a08b34381c97639123afad90aa4769b
chaselancaster <chaseblancaster@gmail.com>
2019-06-12 22:43:48 -0700

    pass name as props to StepThree Component

--- src/App.js
+++ src/App.js
@@ -144,7 +144,7 @@ onTagClick = (currSelectedState, id, text) => {
             {/* <StepProgressBar percent={this.state.percent}/> */}
             </div>
             {/* <StepTwo onTagClick={this.onTagClick} tags={this.state.tags} peopleCount={this.state.peopleCount} peopleNames={this.state.peopleNames} changeHandler={this.changeHandler} physicalDescript={this.state.physicalDescript} changeProgress={this.changeProgress}/> */}
-            <StepThree org={this.state.org} changeHandler={this.changeHandler}/>
+            <StepThree org={this.state.org} changeHandler={this.changeHandler} name={this.state.name}/>
           </> }/>
           <Route exact path={routes.ROOT} render={() => <></> }/>
         </Switch>
