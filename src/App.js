be6c218c5c684eee27f47f06cb98eb1a2269e43e
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-06-13 09:21:36 -0700

    transition add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, {Component }           from 'react';
+import React, { Component } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom'
-import { PacmanLoader }              from 'react-spinners'
+import { PacmanLoader } from 'react-spinners'
+import { CSSTransition } from 'react-transition-group'
 
-import NavBar       from './components/Nav'
+import NavBar from './components/Nav'
 import MapContainer from './components/MapContainer'
-import LearnMore    from './components/LearnMore'
-import DatePicker   from './components/DatePicker'
-import StepProgressBar  from './components/ProgressBar'
+import LearnMore from './components/LearnMore'
+import DatePicker from './components/DatePicker'
+import StepProgressBar from './components/ProgressBar'
 import StepTwo from './components/StepTwo'
 import StepThree from './components/StepThree'
 
 
 
-import * as routes  from './constants/routes'
+import * as routes from './constants/routes'
 import './App.css'
 import 'react-tag-buttons/lib/css/styles.css'
 
@@ -20,6 +21,7 @@ import 'react-tag-buttons/lib/css/styles.css'
 class App extends Component {
 
   state = {
+    showStep: true,
     message: "",
     location: '',
     date: '',
@@ -49,65 +51,65 @@ class App extends Component {
     console.log("changeProgress function hit")
     let currentNum = this.state.percent
     this.setState({
-        percent: currentNum + 50
+      percent: currentNum + 50
     });
-}
-
-onTagClick = (currSelectedState, id, text) => {
-  let {tags} = this.state;
-  if(!currSelectedState){
-      tags.push({id:id, text:text});
-  }else{
-      tags = tags.filter((item)=>{return item.id !== id});
-    // loading: true
   }
-  this.setState({
+
+  onTagClick = (currSelectedState, id, text) => {
+    let { tags } = this.state;
+    if (!currSelectedState) {
+      tags.push({ id: id, text: text });
+    } else {
+      tags = tags.filter((item) => { return item.id !== id });
+      // loading: true
+    }
+    this.setState({
       tags: tags
-  });
-};
+    });
+  };
 
   onSubmit = async e => {
     e.preventDefault();
-    try{
-        const createTicket = await fetch("http://localhost:3001/ticket/create", {
-            method: "POST",
-            // credentials: "include",
-            body: JSON.stringify(this.state),
-            headers: {
-              "Content-Type": "application/json"
-            }
-          });
-          const parsedResponse = await createTicket.json();
-          if (parsedResponse.success) {
-            // setting localStorage on front end so user remains logged in
-            // logout = localStorage.clear()
-            // and clear sessions in back
-            this.setState({
-              message: parsedResponse.message,
-              location: '',
-              date: '',
-              peopleCount: 0,
-              peopleNames: '',
-              physicalDescript: '',
-              needsDescript: '',
-              tags: [],
-              name: '',
-              org: '',
-              selfDescript: '',
-              email: '',
-              phone: '',
-              agreement: false
-            });
-          } 
-    } catch(err) {
-        console.log(err)
+    try {
+      const createTicket = await fetch("http://localhost:3001/ticket/create", {
+        method: "POST",
+        // credentials: "include",
+        body: JSON.stringify(this.state),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      const parsedResponse = await createTicket.json();
+      if (parsedResponse.success) {
+        // setting localStorage on front end so user remains logged in
+        // logout = localStorage.clear()
+        // and clear sessions in back
+        this.setState({
+          message: parsedResponse.message,
+          location: '',
+          date: '',
+          peopleCount: 0,
+          peopleNames: '',
+          physicalDescript: '',
+          needsDescript: '',
+          tags: [],
+          name: '',
+          org: '',
+          selfDescript: '',
+          email: '',
+          phone: '',
+          agreement: false
+        });
+      }
+    } catch (err) {
+      console.log(err)
     }
   };
 
-  render(){
-    // const { loading } = this.state
+  render() {
+    const { showStep } = this.state
 
-    return(
+    return (
 
       <div className="grid-container">
         <div className="grid-mobile"><img src="status_bar.png" /></div>
@@ -121,7 +123,7 @@ onTagClick = (currSelectedState, id, text) => {
           <div className="top-left"><img src="grid_desktop.png"></img></div>
           <div className="mid-left">LAHSA</div><div className="mid-right">Los Angeles Homeless Outreach Portal
         </div>
-        <div className="top-right">Contact</div>
+          <div className="top-right">Contact</div>
           {/* <PacmanLoader color={'gold'} size={10} /> */}
         </div>
 
@@ -132,21 +134,27 @@ onTagClick = (currSelectedState, id, text) => {
         </div>
 
         <div className="grid-main">
-        <Switch>
-          <Route exact path={routes.INFO} render={() =>
-            <LearnMore />
-          }/>
-          <Route exact path={routes.REQUEST} render={() => <>
-            <MapContainer changeHandler={this.changeHandler} location={this.state.location} changeProgress={this.changeProgress}/>
-            <DatePicker />
-            <div className="barContainer">
-            <StepProgressBar percent={this.state.percent}/>
-            </div>
-            <StepTwo onTagClick={this.onTagClick} tags={this.state.tags} peopleCount={this.state.peopleCount} peopleNames={this.state.peopleNames} changeHandler={this.changeHandler} physicalDescript={this.state.physicalDescript} changeProgress={this.changeProgress}/>
-            <StepThree org={this.state.org} changeHandler={this.changeHandler} name={this.state.name} email={this.state.email} phone={this.state.phone} onSubmit={this.onSubmit} />
-          </> }/>
-          <Route exact path={routes.ROOT} render={() => <></> }/>
-        </Switch>
+          <Switch>
+            <Route exact path={routes.INFO} render={() =>
+              <LearnMore />
+            } />
+            <Route exact path={routes.REQUEST} render={() => <>
+              <CSSTransition
+              in={showStep}
+              appear={true}
+              timeout={600}
+              className="fade">
+              <MapContainer changeHandler={this.changeHandler} location={this.state.location} changeProgress={this.changeProgress} />
+              </CSSTransition>
+              <DatePicker />
+              <div className="barContainer">
+                <StepProgressBar percent={this.state.percent} />
+              </div>
+              <StepTwo onTagClick={this.onTagClick} tags={this.state.tags} peopleCount={this.state.peopleCount} peopleNames={this.state.peopleNames} changeHandler={this.changeHandler} physicalDescript={this.state.physicalDescript} changeProgress={this.changeProgress} />
+              <StepThree org={this.state.org} changeHandler={this.changeHandler} name={this.state.name} email={this.state.email} phone={this.state.phone} onSubmit={this.onSubmit} />
+            </>} />
+            <Route exact path={routes.ROOT} render={() => <></>} />
+          </Switch>
         </div>
 
         <div className="grid-footer"></div>
