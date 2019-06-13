ec16b23a9cd2de301bfedf57654b052111459e06
chaselancaster <chaseblancaster@gmail.com>
2019-06-12 22:36:31 -0700

    move org input to StepThree Component

--- src/components/StepThree/index.js
+++ src/components/StepThree/index.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 
-class StepTwo extends Component {
+class StepThree extends Component {
     render() {
         return (
             <div>
                 <h1>This is the StepThree Component</h1>
                 <form>
-                   
-                    
+                    <input type="text" placeholder="org" name="org" onChange={this.props.changeHandler} value={this.props.org}/>
                 </form>
             </div>
         )
