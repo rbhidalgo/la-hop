8c1a14dada96a6861095717e6b0074026ade7d75
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-06-13 15:35:43 -0700

    font-size adjusted

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -9,15 +9,27 @@ import "../Styles/animation.css";
 class NavBar extends Component {
 	state = {
         showStep: true,
-        underline: false
+        underline: true,
+        underline2: true
     };
     
-    changeColor(){
-        this.setState({underline: !this.state.underline})
+    changeColor=()=>{
+        this.setState({
+            underline: !this.state.underline
+        })
+     }
+
+     changeColor2=()=>{
+        this.setState({
+            underline2: !this.state.underline2
+        })
      }
 
 	render() {
-		const { showStep } = this.state;
+        const { showStep } = this.state;
+        let btn_class = this.state.underline ? "nav-link1" : "nav-link2px";
+        let btn_class2 = this.state.underline2 ? "nav-link2" : "nav-link22px";
+
 		return (
 			<CSSTransition
 				in={showStep}
@@ -25,13 +37,13 @@ class NavBar extends Component {
 				timeout={300}
 				classNames='fade'>
                     <nav className='navbar-navigation'>
-    					<div className="nav-link1">
+    					<div className={btn_class} onClick={this.changeColor}>
                         <NavLink
 						to={routes.REQUEST}>
 						Make Request
 					</NavLink>
                     </div>
-                    <div className="nav-link2">
+                    <div className={btn_class2} onClick={this.changeColor2}>
 					<NavLink
 						to={routes.LEARN}>
 						Learn More
