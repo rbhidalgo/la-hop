fb9016e6e3ae929bb2f2d94a75e4c88568c6be26
chaselancaster <chaseblancaster@gmail.com>
2019-06-13 15:26:44 -0700

    add the rest of the icon for learnmore

--- src/components/LearnMore/index.js
+++ src/components/LearnMore/index.js
@@ -12,19 +12,19 @@ class LearnMore extends Component {
                     <p>We’ll use the information from the requests made to dispatch a homeless services outreach team to the area.</p>
                 </div>
                 <div>
-                    <img></img>
+                    <img src="ambulanceicon.png"></img>
                     <p>For medical or mental health emergencies, please call 911.</p>
                 </div>
                 <div>
-                    <img></img>
+                    <img src="policeicon.png"></img>
                     <p>For crime or illegal activity, please contact your local law enforcement agency.</p>
                 </div>
                 <div>
-                    <img></img>
+                    <img src="governmenticon.png"></img>
                     <p>For services like bulky item pickup, illegal dumping or graffiti removal, please contact your municipality.</p>
                 </div>
                 <div>
-                    <img></img>
+                    <img src="callicon.png"></img>
                     <p>LA-HOP does not replace homeless encampment reporting protocols. Please contact your municipality.</p>
                 </div>
             </div>
