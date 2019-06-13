4e3d0fc562e3f9f4b5ff4708628545dc2e1ffeeb
chaselancaster <chaseblancaster@gmail.com>
2019-06-12 22:01:02 -0700

    comitting to pull request

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import TagButtons from 'react-tag-buttons';
+
 
 // // import './Register.css'
 
-const tagsList = [
-    {id:1, text:"Clothing"},
-    {id:2, text:"ID"},
-    {id:3, text:"Mental Health Care"},
-    {id:4, text:"General Assistance"}
-];
+
 
 class Form extends Component {
   
@@ -33,11 +28,7 @@ class Form extends Component {
             <input type="text" placeholder="email" name="email" onChange={this.props.changeHandler} value={email}/>
             <input type="text" placeholder="phone" name="phone" onChange={this.props.changeHandler} value={phone}/>
             <input type="checkbox" name="agreement" />
-            <TagButtons
-                    dataSource={tagsList}
-                    selectedDataSource={tags}
-                    onTagClick={this.props.onTagClick}
-            />
+            
             <button type="submit" onClick={this.props.onSubmit}>Submit</button>
         </form>
   
--- src/components/StepTwo/index.js
+++ src/components/StepTwo/index.js
@@ -6,7 +6,10 @@ const tagsList = [
     {id:1, text:"Clothing"},
     {id:2, text:"ID"},
     {id:3, text:"Mental Health Care"},
-    {id:4, text:"General Assistance"}
+    {id:4, text:"General Assistance"},
+    {id:5, text:"Physical Healthcare"},
+    {id:6, text:"Phone"},
+    {id:7, text:"Food"}
 ];
 
 class StepTwo extends Component {
@@ -17,12 +20,13 @@ class StepTwo extends Component {
                 <form>
                     <input type="text" placeholder="peopleCount" name="peopleCount" onChange={this.props.changeHandler} value={this.props.peopleCount}/>
                     <input type="text" placeholder="peopleNames" name="peopleNames" onChange={this.props.changeHandler} value={this.props.peopleNames}/>
-                    <textarea type="text" placeholder="physicalDescript" name="physicalDescript" onChange={this.props.changeHandler} value={physicalDescript}/>
+                    <textarea type="text" placeholder="physicalDescript" name="physicalDescript" onChange={this.props.changeHandler} value={this.props.physicalDescript}/>
                     <TagButtons
                     dataSource={tagsList}
                     selectedDataSource={this.props.tags}
                     onTagClick={this.props.onTagClick}
-            />
+                    />
+                    <button onClick={this.props.changeProgress}>Next Step</button>
                 </form>
             </div>
         )
