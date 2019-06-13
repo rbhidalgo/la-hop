c1b7be0190355778e39c6ffc0546e228b3b205de
chaselancaster <chaseblancaster@gmail.com>
2019-06-13 16:21:47 -0700

    add ternary statement for stepOne and stepTwo in App.js state

--- src/App.js
+++ src/App.js
@@ -156,9 +156,9 @@ class App extends Component {
             </div>
 
             
-            <StepOne changeHandler={this.changeHandler} location={this.state.location} changeProgress={this.changeProgress}/>
+            {(this.state.stepOne) ? <StepOne changeHandler={this.changeHandler} location={this.state.location} changeProgress={this.changeProgress}/> : null}
 
-            <StepTwo onTagClick={this.onTagClick} tags={this.state.tags} peopleCount={this.state.peopleCount} peopleNames={this.state.peopleNames} changeHandler={this.changeHandler} physicalDescript={this.state.physicalDescript} changeProgress={this.changeProgress}/>
+            {(this.state.stepTwo) ? <StepTwo onTagClick={this.onTagClick} tags={this.state.tags} peopleCount={this.state.peopleCount} peopleNames={this.state.peopleNames} changeHandler={this.changeHandler} physicalDescript={this.state.physicalDescript} changeProgress={this.changeProgress}/> : null }
             
             <StepThree org={this.state.org} changeHandler={this.changeHandler} name={this.state.name} email={this.state.email} phone={this.state.phone} onSubmit={this.onSubmit} />
 
