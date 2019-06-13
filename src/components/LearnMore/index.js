388ebc53dd708a9aae8732a2ab899a9ea57f764a
chaselancaster <chaseblancaster@gmail.com>
2019-06-13 15:35:34 -0700

    Add breaks for for divs in LearnMore

--- src/components/LearnMore/index.js
+++ src/components/LearnMore/index.js
@@ -13,22 +13,27 @@ class LearnMore extends Component {
                     <img src="hearticon.png"></img>
                     <p>We’ll use the information from the requests made to dispatch a homeless services outreach team to the area.</p>
                 </div>
+                <br />
                 <div className="learnChild">
                     <img src="ambulanceicon.png"></img>
                     <p>For medical or mental health emergencies, please call 911.</p>
                 </div>
+                <br />
                 <div className="learnChild">
                     <img src="policeicon.png"></img>
                     <p>For crime or illegal activity, please contact your local law enforcement agency.</p>
                 </div>
+                <br />
                 <div className="learnChild">
                     <img src="governmenticon.png"></img>
                     <p>For services like bulky item pickup, illegal dumping or graffiti removal, please contact your municipality.</p>
                 </div>
+                <br />
                 <div className="learnChild">
                     <img src="callicon.png"></img>
                     <p>LA-HOP does not replace homeless encampment reporting protocols. Please contact your municipality.</p>
                 </div>
+                <br />
             </div>
         )
     }
