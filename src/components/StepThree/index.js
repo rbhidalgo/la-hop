c96cb186a7e7c43f1356c80dd22018bb7c01cba5
chaselancaster <chaseblancaster@gmail.com>
2019-06-12 23:00:59 -0700

    add initial select option input

--- src/components/StepThree/index.js
+++ src/components/StepThree/index.js
@@ -2,11 +2,27 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 
 class StepThree extends Component {
+
+
+
     render() {
         return (
             <div>
                 <h1>This is the StepThree Component</h1>
                 <form>
+
+
+
+
+<select>
+    <option>*** select one***</option>
+    <option name="" value="">value</option>
+</select>
+
+
+
+
+
                     <input type="text" placeholder="org" name="org" onChange={this.props.changeHandler} value={this.props.org}/>
                     <input type="text" placeholder="name" name="name" onChange={this.props.changeHandler} value={this.props.name}/>
                     <input type="text" placeholder="email" name="email" onChange={this.props.changeHandler} value={this.props.email}/>
