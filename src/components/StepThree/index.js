e02a6ff01c77b8e20aa7c2efa36d5acdd8e3bba8
chaselancaster <chaseblancaster@gmail.com>
2019-06-12 22:30:47 -0700

    Add StepThree Component initial structure

---
+++ src/components/StepThree/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
+class StepTwo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the StepThree Component</h1>
+                <form>
+                   
+                    
+                </form>
+            </div>
+        )
+    }
+}
+
+export default StepThree
