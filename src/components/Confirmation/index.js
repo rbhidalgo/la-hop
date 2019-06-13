880eee2abb003a4397f9611a64cf5d9a7dfb7eec
chaselancaster <chaseblancaster@gmail.com>
2019-06-13 14:24:25 -0700

    Add Confirmation Component and initial structure

---
+++ src/components/Confirmation/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Confirmation extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Thank you!</h1>
+                <p>Your request has been received. We appreciate your support in assisting our homeless neighbors.</p>
+                <br />
+                <p>Please visit Learn More to find out what our outreach teams do to support those experiencing homelessness.</p>
+                <br />
+                <button>Back to LAHSA</button>
+                <br />
+                <p>Want to get more involved?</p>
+                <br />
+                <button>Volunteer</button>
+            </div>
+        )
+    }
+}
