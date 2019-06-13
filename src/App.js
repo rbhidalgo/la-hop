cc6504f8bb80aca4f4ae0f89f53fc85c8dca8eb4
chaselancaster <chaseblancaster@gmail.com>
2019-06-13 16:47:53 -0700

    pass stepTwoClick function to StepTwo Component

--- src/App.js
+++ src/App.js
@@ -217,7 +217,7 @@ class App extends Component {
             
             {(this.state.stepOne) ? <StepOne changeHandler={this.changeHandler} location={this.state.location} changeProgress={this.changeProgress} stepOneClick={this.stepOneClick}/> : null}
 
-            {(this.state.stepTwo) ? <StepTwo onTagClick={this.onTagClick} tags={this.state.tags} peopleCount={this.state.peopleCount} peopleNames={this.state.peopleNames} changeHandler={this.changeHandler} physicalDescript={this.state.physicalDescript} changeProgress={this.changeProgress}/> : null }
+            {(this.state.stepTwo) ? <StepTwo onTagClick={this.onTagClick} tags={this.state.tags} peopleCount={this.state.peopleCount} peopleNames={this.state.peopleNames} changeHandler={this.changeHandler} physicalDescript={this.state.physicalDescript} changeProgress={this.changeProgress} stepTwoClick={this.stepTwoClick}/>/> : null }
             
             {(this.state.stepThree) ? <StepThree org={this.state.org} changeHandler={this.changeHandler} name={this.state.name} email={this.state.email} phone={this.state.phone} onSubmit={this.onSubmit} /> : null }
 
