a83c622002fe92d9081393ce581de41a4471e29b
chaselancaster <chaseblancaster@gmail.com>
2019-06-13 15:30:40 -0700

    add className of learnChild to each child div in LearnMore

--- src/components/LearnMore/index.js
+++ src/components/LearnMore/index.js
@@ -9,23 +9,23 @@ class LearnMore extends Component {
         return(
 
             <div className="learnMoreContainer">
-                <div>
+                <div className="learnChild">
                     <img src="hearticon.png"></img>
                     <p>We’ll use the information from the requests made to dispatch a homeless services outreach team to the area.</p>
                 </div>
-                <div>
+                <div className="learnChild">
                     <img src="ambulanceicon.png"></img>
                     <p>For medical or mental health emergencies, please call 911.</p>
                 </div>
-                <div>
+                <div className="learnChild">
                     <img src="policeicon.png"></img>
                     <p>For crime or illegal activity, please contact your local law enforcement agency.</p>
                 </div>
-                <div>
+                <div className="learnChild">
                     <img src="governmenticon.png"></img>
                     <p>For services like bulky item pickup, illegal dumping or graffiti removal, please contact your municipality.</p>
                 </div>
-                <div>
+                <div className="learnChild">
                     <img src="callicon.png"></img>
                     <p>LA-HOP does not replace homeless encampment reporting protocols. Please contact your municipality.</p>
                 </div>
