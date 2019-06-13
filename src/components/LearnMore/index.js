5580ec88ccadc1a54019a2f66168417bea2244e7
chaselancaster <chaseblancaster@gmail.com>
2019-06-13 15:23:03 -0700

    add all paragraph info in learn more and test one image

--- src/components/LearnMore/index.js
+++ src/components/LearnMore/index.js
@@ -7,7 +7,26 @@ class LearnMore extends Component {
         return(
 
             <div>
-                <h1 style={{marginTop: "40px"}}>Learn More</h1>
+                <div>
+                    <img src="..../public/hearticon.png"></img>
+                    <p>We’ll use the information from the requests made to dispatch a homeless services outreach team to the area.</p>
+                </div>
+                <div>
+                    <img></img>
+                    <p>For medical or mental health emergencies, please call 911.</p>
+                </div>
+                <div>
+                    <img></img>
+                    <p>For crime or illegal activity, please contact your local law enforcement agency.</p>
+                </div>
+                <div>
+                    <img></img>
+                    <p>For services like bulky item pickup, illegal dumping or graffiti removal, please contact your municipality.</p>
+                </div>
+                <div>
+                    <img></img>
+                    <p>LA-HOP does not replace homeless encampment reporting protocols. Please contact your municipality.</p>
+                </div>
             </div>
         )
     }
