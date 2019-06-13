3b211527754c8fc9b17213a714154d8d9a750d85
chaselancaster <chaseblancaster@gmail.com>
2019-06-12 21:14:25 -0700

    create Step1 Component

---
+++ src/components/Step 1 /index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
+class Step1 extends Component {
+    render() {
+        return (
+            <div>This is the Step 1 component</div>
+        )
+    }
+}
